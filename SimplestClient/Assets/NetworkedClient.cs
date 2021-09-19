67a96452313f0715b1b8772bb81496b1fe1de9c1
Delta <60995006+Delta-Sama@users.noreply.github.com>
2021-09-19 17:15:21 +0300

    Commented lines

--- SimplestClient/Assets/NetworkedClient.cs
+++ SimplestClient/Assets/NetworkedClient.cs
@@ -42,8 +42,10 @@ public class NetworkedClient : MonoBehaviour
             byte[] recBuffer = new byte[1024];
             int bufferSize = 1024;
             int dataSize;
+            // Receive a message and put it into a byte recBuffer
             NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recHostID, out recConnectionID, out recChannelID, recBuffer, bufferSize, out dataSize, out error);
 
+            // Process received event depending on its type
             switch (recNetworkEvent)
             {
                 case NetworkEventType.ConnectEvent:
@@ -51,6 +53,7 @@ public class NetworkedClient : MonoBehaviour
                     ourClientID = recConnectionID;
                     break;
                 case NetworkEventType.DataEvent:
+                    // Decrypt the message from bytes to string
                     string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
                     ProcessRecievedMsg(msg, recConnectionID);
                     //Debug.Log("got msg = " + msg);
@@ -70,16 +73,22 @@ public class NetworkedClient : MonoBehaviour
         {
             Debug.Log("Attempting to create connection");
 
+            // Initialize NetworkTransport before its usage
             NetworkTransport.Init();
 
+            // Create a config which describes channel types, various timeouts and sizes
             ConnectionConfig config = new ConnectionConfig();
-            reliableChannelID = config.AddChannel(QosType.Reliable);
-            unreliableChannelID = config.AddChannel(QosType.Unreliable);
+            reliableChannelID = config.AddChannel(QosType.Reliable); // Guarantee of delivering but not ordering
+            unreliableChannelID = config.AddChannel(QosType.Unreliable); // No guarantee of delivering or ordering
+
+            // Define the number of default connections and number of special connections
             HostTopology topology = new HostTopology(config, maxConnections);
+            // Add a host based on our topology and bind the socket to a random port (since 0 as a port is provided)
             hostID = NetworkTransport.AddHost(topology, 0);
             Debug.Log("Socket open.  Host ID = " + hostID);
 
-            connectionID = NetworkTransport.Connect(hostID, "192.168.2.37", socketPort, 0, out error); // server is local on network
+            // Connect to a peer with a chosen address and socket
+            connectionID = NetworkTransport.Connect(hostID, "192.168.0.11", socketPort, 0, out error); // server is local on network
 
             if (error == 0)
             {
@@ -93,12 +102,15 @@ public class NetworkedClient : MonoBehaviour
     
     public void Disconnect()
     {
+        // Send a peer a message of disconnection and disconnect
         NetworkTransport.Disconnect(hostID, connectionID, out error);
     }
     
     public void SendMessageToHost(string msg)
     {
+        // Convert a string to a byte buffer
         byte[] buffer = Encoding.Unicode.GetBytes(msg);
+        // Send a message(byte buffer) to the peer
         NetworkTransport.Send(hostID, connectionID, reliableChannelID, buffer, msg.Length * sizeof(char), out error);
     }
 
