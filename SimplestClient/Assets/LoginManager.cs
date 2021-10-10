6bff7cff4e8c3509a11044621d32ffcda3bb4922
Delta <60995006+Delta-Sama@users.noreply.github.com>
2021-10-10 20:47:26 +0300

    New separate interface for login and account create

--- SimplestClient/Assets/LoginManager.cs
+++ SimplestClient/Assets/LoginManager.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
-    GameObject submitButton, userNameInput, passwordInput, emailInput, loginToggle, createToggle;
+    [SerializeField] GameObject createTab, loginTab;
+
+    GameObject submitButton, userNameInput, passwordInput, toggles, loginToggle, createToggle;
+    GameObject forgotPasswordButton, createLoginInput, createPasswordInput, createEmailInput;
 
     // Start is called before the first frame update
     void Start()
     {
+        createTab.SetActive(true);
+
         GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
 
         foreach (GameObject obj in allObjects)
@@ -20,18 +25,29 @@ public class LoginManager : MonoBehaviour
                 userNameInput = obj;
             else if (obj.name == "PasswordInputField")
                 passwordInput = obj;
-            else if (obj.name == "EmailInputField")
-                emailInput = obj;
+            else if (obj.name == "CreateLoginInputField")
+                createLoginInput = obj;
+            else if (obj.name == "CreatePasswordInputField")
+                createPasswordInput = obj;
+            else if (obj.name == "CreateEmailInputField")
+                createEmailInput = obj;
+            else if (obj.name == "Toggles")
+                toggles = obj;
             else if (obj.name == "LoginToggle")
                 loginToggle = obj;
             else if (obj.name == "CreateAccountToggle")
                 createToggle = obj;
         }
 
+        createTab.SetActive(false);
+
         submitButton.GetComponent<Button>().onClick.AddListener(SubmitRequst);
 
+        loginToggle.GetComponent<Toggle>().onValueChanged.AddListener(AdjustUI);
+        createToggle.GetComponent<Toggle>().onValueChanged.AddListener(AdjustUI);
 
     }
+
 
     // Update is called once per frame
     void Update()
@@ -42,20 +58,30 @@ public class LoginManager : MonoBehaviour
     void SubmitRequst()
     {
         Debug.Log("Submit");
-        string login = userNameInput.GetComponent<InputField>().text;
-        string password = passwordInput.GetComponent<InputField>().text;
-        string email = emailInput.GetComponent<InputField>().text;
 
         if (loginToggle.GetComponent<Toggle>().isOn)
         {
+            string login = userNameInput.GetComponent<InputField>().text;
+            string password = passwordInput.GetComponent<InputField>().text;
+
             if (login != "" && password != "")
                 NetworkedClient.Instance.SendMessageToHost(ClientToServerTransferSignifiers.Login + "," + login + "," + password);
         }
         else if (createToggle.GetComponent<Toggle>().isOn)
         {
+            string login = createLoginInput.GetComponent<InputField>().text;
+            string password = createPasswordInput.GetComponent<InputField>().text;
+            string email = createEmailInput.GetComponent<InputField>().text;
+
             if (login != "" && password != "" && email != "")
                 NetworkedClient.Instance.SendMessageToHost(ClientToServerTransferSignifiers.CreateAccount + "," + login + "," + password + "," + email);
         }
     }
 
+    void AdjustUI(bool _)
+    {
+        createTab.SetActive(createToggle.GetComponent<Toggle>().isOn);
+        loginTab.SetActive(loginToggle.GetComponent<Toggle>().isOn);
+
+    }
 }
